2bfc4481129e8295ccab3131517504b8c122c801
arekhn <arekhn@gmail.com>
2020-06-28 19:20:12 -0600

    Revert "Merge branch 'master' of https://github.com/AngelRisso1337z/paro_app"
    This reverts commit a80e97d3a3a5dc4f2b84ca19bbdb536d696d18a1, reversing
    changes made to 4d76c3c6f6b6be8e541ea43e33eadff83e1f3031.

--- js/my-app.js
+++ js/my-app.js
@@ -1,85 +1,87 @@
 // Initialize your app
 var app = new Framework7({
-    id: 'com.app',
-    root: '#app',
-    theme: 'md',
-    view: {
+	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
         pushState: false
     },
-    cache: false,
-    cacheDuration: 0,
-    modalTitle: 'Delivery Food',
-    dialog: {
-        title: 'Delivery Food',
-        buttonOk: 'Aceptar',
-    },
-    routes: [{
-            path: '/home/',
-            url: 'index.html',
-            name: 'home',
-        },
-        {
-            path: '/about/',
-            url: 'about.html',
-            name: 'about',
-        },
-        {
-            path: '/perfil/',
-            url: 'perfil.html',
-            name: 'perfil',
-        },
-        {
-            path: '/Ryan/',
-            url: 'Ryan.html',
-            name: 'Ryan',
-        },
-        {
-            path: '/Angel/',
-            url: 'Angel.html',
-            name: 'Angel',
-        },
-        {
-            path: '/Juan/',
-            url: 'Juan.html',
-            name: 'Juan',
-        },
-        {
-            path: '/Alexis/',
-            url: 'Alexis.html',
-            name: 'Alexis',
-        },
-        {
-            path: '/informacion/',
-            url: 'informacion.html',
-            name: 'informacion',
-        }, {
-            path: '/trabajadores/',
-            url: 'trabajadores.html',
-            name: 'trabajadores',
-        }
-    ]
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'Delivery Food',
+	dialog: {
+		title: 'Delivery Food',
+		buttonOk: 'Aceptar',
+  	},
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/about/',
+    	url: 'about.html',
+    	name: 'about',
+  		},
+		{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		},
+		{
+		path: '/Ryan/',
+    	url: 'Ryan.html',
+    	name: 'Ryan',
+  		},
+		{
+		path: '/Angel/',
+    	url: 'Angel.html',
+    	name: 'Angel',
+  		},
+		{
+		path: '/Juan/',
+    	url: 'Juan.html',
+    	name: 'Juan',
+  		},
+		{
+		path: '/Alexis/',
+    	url: 'Alexis.html',
+    	name: 'Alexis',
+  		},
+		{
+		path: '/informacion/',
+    	url: 'informacion.html',
+    	name: 'informacion',
+  		}
+	]
 });
 
 // Export selectors engine
 var $$ = Dom7;
 
 
-$$('#btnAlerta').on('click', function() {
-    app.dialog.alert('Hola Diseño Web 2');
+$$('#btnAlerta').on('click',function(){
+	app.dialog.alert('Hola Diseño Web 2');
 });
 
-$$('#btnLogin').on('click', function(e) {
-    e.preventDefault();
-
-    var usuario = $$('#username').val();
-    var password = $$('#password').val();
-    console.log(usuario)
-    console.log(password)
-    if (usuario == 'admin' && password == "admin") {
-        $$('#nombre').html(usuario);
-        app.loginScreen.close('.login-screen');
-    } else {
-        app.dialog.alert('Datos ingresados no son validos');
-    }
-
+$$('#btnLogin').on('click', function(e){
+	e.preventDefault();
+	
+	var usuario = $$('#username').val();
+	var password = $$('#password').val();
+	console.log(usuario)
+	console.log(password)
+	if (usuario == 'admin' && password == "admin"){
+		$$('#nombre').html(usuario);
+		app.loginScreen.close('.login-screen');
+	}else{
+			app.dialog.alert('Datos ingresados no son validos');
+	}  	
+  
 });
+
+
+
+ 
+
